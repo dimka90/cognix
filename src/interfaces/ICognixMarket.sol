56f494433592b7fc5e7157057933265b5457ddb5
dimka90 <dimkayilrit@gmail.com>
2026-01-12 23:43:51 +0100

    feat: add Task struct definition

--- src/interfaces/ICognixMarket.sol
+++ src/interfaces/ICognixMarket.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.8.19;
 
 interface ICognixMarket {
     enum TaskStatus { Created, Assigned, ProofSubmitted, Completed, Cancelled, Disputed }
+
+    struct Task {
+        address employer;
+        address assignee;
+        string metadataURI;
+        uint256 reward;
+        TaskStatus status;
+        uint256 createdAt;
+        uint256 updatedAt;
+    }
 }
