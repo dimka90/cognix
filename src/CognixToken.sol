600a0bd24eec75f72b3e0cb7b5f21947fd7c98f9
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-16 13:07:40 +0100

    refactor: make totalSupply immutable for gas savings

--- src/CognixToken.sol
+++ src/CognixToken.sol
@@ -7,7 +7,7 @@ contract CognixToken {
     string public name = "Cognix Token";
     string public symbol = "CGX";
     uint8 public constant decimals = 18;
-    uint256 public totalSupply;
+    uint256 public immutable totalSupply;
     
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
