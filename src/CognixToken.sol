3e19eee2fd16c89f653035689a485ae58e988818
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-01-16 13:07:25 +0100

    refactor: make decimals constant for gas optimization

--- src/CognixToken.sol
+++ src/CognixToken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.19;
 contract CognixToken {
     string public name = "Cognix Token";
     string public symbol = "CGX";
-    uint8 public decimals = 18;
+    uint8 public constant decimals = 18;
     uint256 public totalSupply;
     
     mapping(address => uint256) public balanceOf;
