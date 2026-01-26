6ecf75b57dddcf67314c54ef1d8f488a75012063
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-26 19:25:01 +0100

    Add CognixToken_2 smart contract

---
+++ contracts/CognixToken_2.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CognixToken2 {
+    string public name = "CognixToken 2";
+    uint256 public initialSupply;
+    constructor() { initialSupply = 1000; }
+}
