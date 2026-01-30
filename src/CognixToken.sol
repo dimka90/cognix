1de94f2ac23507cdf10e382d6cc619a4bfcc8de6
dimka90 <dimkayilrit@gmail.com>
2026-01-30 05:32:41 +0100

    Feat: Integrate Ownable access control for administrative tasks

--- src/CognixToken.sol
+++ src/CognixToken.sol
@@ -1,28 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CognixToken {
+contract CognixToken is Ownable {
     string public name;
     string public symbol;
     uint8 public constant decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory _name, string memory _symbol, uint256 _supply, address _owner) {
+    constructor(string memory _name, string memory _symbol, uint256 _supply, address _owner) Ownable(_owner) {
         name = _name;
         symbol = _symbol;
         totalSupply = _supply;
         balanceOf[_owner] = _supply;
-    }
-
-    function transfer(address to, uint256 amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Low balance");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        emit Transfer(msg.sender, to, amount);
-        return true;
     }
 }
